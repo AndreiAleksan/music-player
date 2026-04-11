19d4c06629840b278a3365e26a5ce0bd6f7b8abe
Andrei_Aleksan <andreiaragaoaleks@gmail.com>
2026-04-10 21:47:54 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+TODO 
+
+- [x] Create sound files dictionary
+- [ ] Define "pause" character
+- [ ] Make a function to break down and read the text file
+- [ ] Convert the broken down text file into a song
+
---
+++ musicplayer.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.mixer.init()
+notes = {
+    "C" : pygame.mixer.Sound("notes/A3.wav"),
+    "D" : pygame.mixer.Sound("notes/D3.wav"),
+    "E" : pygame.mixer.Sound("notes/E3.wav"),
+    "F" : pygame.mixer.Sound("notes/F3.wav"),
+    "G" : pygame.mixer.Sound("notes/G3.wav"),
+    "A" : pygame.mixer.Sound("notes/A3.wav"),
+    "B" : pygame.mixer.Sound("notes/B3.wav")
+    "-" : 
+}
+
+file_name = input("Please input the path of the text file you wish to play \n")
